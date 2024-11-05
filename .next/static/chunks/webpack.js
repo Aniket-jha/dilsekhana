c5ec1b4cb27ecfd841a5ded75d10cadc66572631
vishal <vishalsanap663@gmail.com>
2024-11-05 13:51:22 +0530

    cart functionality updated

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "383a4295324ae066"; }
+/******/ 		__webpack_require__.h = function() { return "574256cf796b1cd1"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
